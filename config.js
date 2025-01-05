d1ed29413d773f4da23d6f910ce2f039fab8865c
itzlightkira <itzyoshan@gmail.com>
2025-01-05 23:04:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ import axios from 'axios'
 // Global variables
 global.owner = [
   ['918360234087', 'Ｗ Ｈ Ｉ Ｔ Ｅ ４ ４ ４  ? ?', true], 
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['923181491684', 'Eren Yeagerシ', true], 
   ['233533763772', 'Abraham Dwamena2.0', true],
   ['233268374753', 'Abraham Dwamena3.0', true]
 ] 
 
 global.mods = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.prems = ['923184070915', '94789481495', '918360234087', '233268374753']
+global.prems = ['923181491684', '94789481495', '918360234087', '233268374753']
 global.allowed = ['923184070915', '233268374753', '918360234087', '233268374753']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
@@ -51,8 +51,8 @@ global.APIKeys = {
 // Sticker WM
 global.botname = '𝘟𝘭𝘪𝘤𝘰𝘯 𝘝𝘰𝘭 2'
 global.premium = 'true'
-global.packname = '𝙏𝙚𝙖𝙢 𝙓𝙇𝙄𝘾𝙊𝙉' 
-global.author = '@𝘮𝘥𝘹𝘭𝘪𝘤𝘰𝘯𝘷2' 
+global.packname = 'Eren YEAGER' 
+global.author = 'Made By Bє⍺𝗌τ Ꭲıτaⲛ ☄' 
 global.menuvid = 'https://files.catbox.moe/yu3j54.mp4'
 global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
